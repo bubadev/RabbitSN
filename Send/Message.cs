072a802b86704b7d8b8013d7cf8cab89c8b3ffca
bubadev <idioms999@gmail.com>
2022-06-15 17:44:05 +0300

    fix

--- Send/Message.cs
+++ Send/Message.cs
@@ -31,7 +31,6 @@ namespace RabbitSN
             using (Config config = new Config())
             {
                 Time = DateTime.Now;
-
                 string json = JsonSerializer.Serialize(this);
                 byte[] body = Encoding.UTF8.GetBytes(json);
 
